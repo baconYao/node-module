9dbde689ab9426f4b6c65258890d44ef0ec65041
baconYao <bacon735392@gmail.com>
2017-08-25 10:50:18 +0800

    Add es6 filter

--- es6/filter/index.js
+++ es6/filter/index.js
@@ -28,3 +28,20 @@ var conditionOutput = products.filter((product) => {
 });
 
 console.log(conditionOutput);
+
+
+
+var post = { id: 4, title: 'New Post'};
+var comments = [
+  {postId: 4, content: 'djasbn'},
+  {postId: 3, content: '12315'},
+  {postId: 4, content: '!&#%$*!@'}
+];
+
+function commentsForPost(post, comments) {
+  return comments.filter(function(comment) {
+    return comment.postId === post.id;
+  });
+};
+
+console.log(commentsForPost(post, comments));
