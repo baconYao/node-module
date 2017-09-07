6a3da3704714843a12238c6fc598df1844971459
baconYao <bacon735392@gmail.com>
2017-09-07 11:03:48 +0800

    Add es6 arrow function

---
+++ es6/arrow_function/index.js
@@ -0,0 +1,14 @@
+const add = (a, b) => {
+  return a + b;
+};
+
+console.log(add(1,2));
+
+
+const add2 = (a, b) => a + b;
+
+console.log(add2(1,2));
+
+
+const double = number => 2 * number;
+console.log(double(8));
